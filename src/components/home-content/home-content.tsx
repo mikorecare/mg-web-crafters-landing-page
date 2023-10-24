6f8015a89d6a0913d0ca39bfb9712e23967b9c86
SvenSven <mikorecare@gmail.com>
2023-10-25 05:56:35 +0800

    FEAT: Added background picture

--- src/components/home-content/home-content.tsx
+++ src/components/home-content/home-content.tsx
@@ -1,5 +1,5 @@
 import '../../styles/home-content.css'
-import HomePic from '../../assets/home-content-pic.png'
+import HomePic from '../../assets/home-content-main-pic.png'
 const OurBrand = () => {
 
 const content ="We are your one-stop destination for all things digital. Our mission is simple: to empower businesses and individuals to thrive in the ever-evolving online landscape. We combine cutting-edge technology, creative expertise, and unwavering commitment to help you achieve your digital goals."
