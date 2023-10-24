9e60a5fd5f04ab30b927eff8c98c312e552483f6
SvenSven <mikorecare@gmail.com>
2023-10-25 02:15:36 +0800

    FEAT: Updated footer components and css

--- src/components/homepage/footer.tsx
+++ src/components/homepage/footer.tsx
@@ -1,15 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faTwitter, faLinkedin, faGoogle, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import '../../styles/footer.css'
+
+const SocialIcons = () =>{
+  return(
+    <div className="social-icons">
+      <FontAwesomeIcon icon={faFacebook} className='icon' />
+      <FontAwesomeIcon icon={faTwitter} className='icon'/>
+      <FontAwesomeIcon icon={faLinkedin} className='icon'/>
+      <FontAwesomeIcon icon={faGoogle} className='icon'/>
+      <FontAwesomeIcon icon={faInstagram} className='icon'/>
+    </div>
+  )
+
+}
+
+const Copyright = () =>{
+  return(
+    <div className="copyright">&copy; 2010 - 2023 MGWebCrafters-VA. All Rights Reserved.</div>
+  )
+}
+
 const Footer = () => {
     return (
       <footer className="footer">
-        <div className="social-icons">
-          <FontAwesomeIcon icon={faFacebook} />
-          <FontAwesomeIcon icon={faTwitter} />
-          <FontAwesomeIcon icon={faLinkedin} />
-          {/* Add more social media icons as needed */}
-        </div>
+        <SocialIcons/>
+        <Copyright/>
       </footer>
     );
   };
