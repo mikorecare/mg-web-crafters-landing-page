b45b4dcfe5cfa4670e9ca563564a82b14347e5f3
SvenSven <mikorecare@gmail.com>
2023-10-25 04:49:07 +0800

    FEAT: HomeComponent

---
+++ src/components/homepage/home-content.tsx
@@ -0,0 +1,11 @@
+import '../../styles/home-content.css'
+
+const HomeContent = () => {
+    return (
+        <div className="home-content">
+
+        </div>
+    )
+}
+
+export default HomeContent;
--- src/components/homepage/home.tsx
+++ src/components/homepage/home.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./navbar"
 import Footer from "./footer"
+import HomeContent from "./home-content"
 export default function Home(){
     return(
         <>
             <Navbar/>
+            <HomeContent/>
             <Footer/>
         </>
     )
