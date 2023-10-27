76aa70371149fd063a2cc9fe378455adfe811637
SvenSven <mikorecare@gmail.com>
2023-10-28 05:12:26 +0800

    FEAT: Contact us component

--- src/components/home-content/home-content.tsx
+++ src/components/home-content/home-content.tsx
@@ -6,7 +6,8 @@ import { faCode } from '@fortawesome/free-solid-svg-icons';
 import { faMailBulk } from '@fortawesome/free-solid-svg-icons/faMailBulk';
 import { faSearchDollar } from '@fortawesome/free-solid-svg-icons/faSearchDollar';
 import { faTty } from '@fortawesome/free-solid-svg-icons/faTty';
-import AnimatedPic from '../../assets/Animated-logo.png'
+import WhyUsPic from '../../assets/Animated-logo.png'
+import ContactUsPic from '../../assets/contact-us-graphics.png'
 
 const OurBrand = () => {
     let content ="MGWebCrafters was founded by a group of forward-thinkers who recognized the transformative power of the internet. We've seen the web evolve from a simple communication tool to a dynamic marketplace, and we've been a part of this evolution every step of the way. Our journey is marked by continuous learning, adaptability, and an unwavering commitment to innovation."
@@ -51,8 +52,8 @@ const OurServices = () =>{
 
     return(
         <section className='our-services'>
-            <h1 className='our-services-title'>Our Services</h1>
-            <ul>
+            <h1 className='our-services-title' id="our-services">Our Services</h1>
+            <ul >
                 {services.map((list,index)=>(
                     <li key={index}>
                         <div className='service-info'>
@@ -71,7 +72,7 @@ const OurServices = () =>{
 const WhyUs = () =>{
     let whyUs = "At MGWebCrafters, we're more than just a digital services company. We are a passionate team of creators, strategists, and problem solvers dedicated to helping businesses and individuals succeed in the digital world."
     return(
-        <section className='why-us'>
+        <section className='why-us' id="about">
             <div className='why-us-content'>
                 <div>
                     <h1 className='why-us-title'>Why Us?</h1>
@@ -79,7 +80,31 @@ const WhyUs = () =>{
                 </div>
 
      
-                <img src={AnimatedPic}/>
+                <img src={WhyUsPic}/>
+            </div>
+            
+        </section>
+    )
+}
+
+const ContactUs = () =>{
+    let contact = {email:
+    "mgwebcraftersva@gmail.com",
+    phone_1:
+    "+63 916-965-3845",
+    phone_2:"+63 976-386-0567"}
+    return(
+        <section className='contact-us' id="about">
+            <div className='contact-us-content' id="contact-us">
+                <img src={ContactUsPic}/>
+                <div>
+                    <h1 className='contact-us-title'>Contact Us</h1>
+                    <p className='contact-us-email'>Email: <span className='contact-us-info'>{contact.email}</span></p>
+                    <p className='contact-us-phone'>Phone: <span className='contact-us-info'>{contact.phone_1} <br/> {contact.phone_2}</span></p>
+                </div>
+
+     
+                
             </div>
             
         </section>
@@ -92,6 +117,7 @@ const HomeContent = () => {
             <OurBrand/>
             <OurServices/>
             <WhyUs/>
+            <ContactUs/>
         </div>
     )
 }
