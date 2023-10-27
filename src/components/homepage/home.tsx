ca410f1799f2d002ad98c1587960c31632c4f85f
SvenSven <mikorecare@gmail.com>
2023-10-28 02:39:47 +0800

    FEAT: Removed zooming out and in on page

--- src/components/homepage/home.tsx
+++ src/components/homepage/home.tsx
@@ -1,11 +1,35 @@
+import { useEffect } from 'react';
 import Navbar from "./navbar"
 import Footer from "./footer"
 import HomeContent from "../home-content/home-content"
 export default function Home(){
+
+    useEffect(() => {
+        const handleWheel = (e: WheelEvent) => {
+            if (e.ctrlKey === true) {
+                e.preventDefault(); // Prevent default zoom behavior when the control key is pressed
+            }
+        };
+
+        const homeContentElement = document.getElementById('home-content');
+
+        if (homeContentElement) {
+            homeContentElement.addEventListener('wheel', handleWheel, { passive: false });
+
+            // Cleanup by removing the event listener on component unmount
+            return () => {
+                homeContentElement.removeEventListener('wheel', handleWheel);
+            };
+        }
+    }, []);
+    
     return(
         <>
             <Navbar/>
-            <HomeContent/>
+            <div id="home-content">
+                <HomeContent/>
+            </div>
+            
             <Footer/>
         </>
     )
